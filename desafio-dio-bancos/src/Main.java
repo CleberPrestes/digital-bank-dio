574812d793e37de021b29b300e3a520aa70edd01
Cleber Prestes <cleberprestes@hotmail.com>
2022-07-18 12:14:22 -0300

    first commit

---
+++ desafio-dio-bancos/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("Pedro");
+		
+		
+		Conta cc = new ContaCorrente(cliente1);
+	
+		cc.depositar(100);
+		
+		
+		
+		Conta cp = new ContaPoupanca(cliente1);
+		cc.transferir(100, cp);
+		
+		cc.imprimirExtrato();
+		cp.imprimirExtrato();
+
+	}
+
+}
